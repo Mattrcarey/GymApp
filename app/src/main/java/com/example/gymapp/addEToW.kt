1fce2325c2afbd1c4ed3efd570c5cc5c26ec1e46
Mattrcarey <Matthewreececarey1999@gmail.com>
2021-03-05 17:36:38 -0500

    Created a fragment that will display all the exercises with a button at the bottom to add selected to the workout, the next step is to allow the user to select exercises and to write the code for the button

--- app/src/main/java/com/example/gymapp/addEToW.kt
+++ app/src/main/java/com/example/gymapp/addEToW.kt
@@ -1,10 +1,16 @@
 package com.example.gymapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +23,66 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class addNewEToW : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_e_to_w, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_add_e_to_w, container, false)
+        val btnAdd : Button = view.findViewById<Button>(R.id.btnAdd)
+        btnAdd.setOnClickListener { view ->
+            addRecord()
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment addNewEToW.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            addNewEToW().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        val applicationContext = requireContext().applicationContext
+        viewExercises()
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val mContext = context
+    }
+
+    private fun viewExercises(){
+        val applicationContext = requireContext().applicationContext
+        val exercisesList : ArrayList<Exercises> =  MainActivity.databaseHandler.getExercises(applicationContext)
+        val adapter =  ExerciseAdapter(applicationContext, exercisesList)
+        val rv : RecyclerView = requireView().findViewById(R.id.rvItemsList)
+        rv.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, true ) as RecyclerView.LayoutManager
+        rv.adapter = adapter
+    }
+
+    private fun addRecord() {
+        val applicationContext = activity?.applicationContext
+        val etName = view?.findViewById<EditText>(R.id.etName)
+        val name = etName?.text.toString()
+        if (name.isNotEmpty()) {
+            val exercises = Exercises()
+            exercises.exerciseName = name
+            if (applicationContext != null) {
+                MainActivity.databaseHandler.addExercise(applicationContext, exercises)
             }
+
+            Toast.makeText(applicationContext, name, Toast.LENGTH_LONG).show()
+            etName?.text?.clear()
+
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Name cannot be blank",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        viewExercises()
+    }
+
+    override fun onResume() {
+        viewExercises()
+        super.onResume()
     }
 }
